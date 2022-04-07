f9a59e66ca691fda11a554b9726e234ecb35f2db
faisal-inam <faisal.inam@gmail.com>
2022-04-07 13:43:40 +0500

    few changes

--- hello.java
+++ hello.java
@@ -1,4 +1,7 @@
 public class hello{
     public static void main(string [])
-    System.out.println("Welcome to ACP");                             
+    System.out.println("Welcome to ACP");  
+    System.out.println("Welcome to ACP");
+    System.out.println("Welcome to ACP");
+    
 }
