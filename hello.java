73513696db89e50819de253f8893641167f9e8e4
faisal-inam <87889202+faisal-inam@users.noreply.github.com>
2022-04-07 13:44:56 +0500

    Update hello.java
    Few changes

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(string [])
     System.out.println("Welcome to ACP");  
-    System.out.println("Welcome to ACP");
-    System.out.println("Welcome to ACP");
+    System.out.println("Welcome to Git Lecture");
+    System.out.println("Welcome to OOP");
     
 }
